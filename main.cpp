a72b08a39377a27a61cf56c7af4d64a6f2e21c77
Dennis <whyarewestillherejust@gmail.com>
2025-01-29 02:54:46 +0300

    sdl callbacks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+#define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
 
-auto main(int argc, char** argv) -> int
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
 {
-	return 0;
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+	return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+
 }
